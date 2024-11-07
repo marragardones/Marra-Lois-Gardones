aea16948456419ce65a5e6c9454a6514055de443
marragardones <145832161+marragardones@users.noreply.github.com>
2024-11-07 17:24:58 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -13,25 +13,22 @@
 
 <body>
 
-<div class="container">
-    <label>Firtsname></label>
-    <input type="text" name="firsname" class="form-control">
+<p>Ubuntu Server-PHP deployment- Marra Gardones</p>
 
-</div>
 
-<div class="container">
-    <label>middle</label>
-    <input type="text" name="firsname" class="form-control">
-
-</div>
-
-<div class="container">
-    <label>lastnamename></label>
-    <input type="text" name="firsname" class="form-control">
-
-</div>
-
-    <p>Ubuntu Server-PHP deployment - Marra Gardones</p>
+<form action="/action_page.php" method="get">
+  <label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="lname">Middle name:</label>
+  <label for="fname">Last name:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <input type="text" id="lname" name="lname"><br><br>
+   <input type="text" id="mname" name="mname"><br><br>
+   <input type="text" id="lname" name="lname"><br><br>
+  <button type="submit">Submit</button>
+  <button type="submit" formaction="/action_page2.php">Submit to another page</button>
+</form>
+    
 </body>
 </html>
 
